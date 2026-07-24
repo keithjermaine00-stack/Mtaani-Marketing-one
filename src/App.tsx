03bf1ae0f4e589f517cd74c86745edf0782d7944
Mtaani-mark <keithjermaine00@gmail.com>
2026-07-24 11:15:46 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,12 @@ import Pricing from '@/components/Pricing';
 import FAQ from '@/components/FAQ';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
-import ClientsMarquee from '@/components/ClientsMarquee';
 import ServiceDetail from '@/pages/ServiceDetail';
 
 function Home() {
   return (
     <>
       <Hero />
-      <ClientsMarquee />
       <Services />
       <About />
       <WhyUs />
